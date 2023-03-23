ecde011fa8762c69e1294524886a1b52f166116c
mmdreza <m13r81a@gmail.come>
2023-03-23 11:49:04 +0430

    resume section

--- script/app.js
+++ script/app.js
@@ -1,6 +1,7 @@
 let toggleIcon  = document.querySelector(".nav__toggle-icon");
 let menu  = document.querySelector(".menu");
 let cover  = document.querySelector(".cover");
+let resumeListItmes = document.querySelectorAll(".resume-list__itme");
 
 
 toggleIcon.addEventListener("click" , function(){
@@ -9,3 +10,18 @@ toggleIcon.addEventListener("click" , function(){
     menu.classList.toggle("menu--open")
     cover.classList.toggle("cover--open")
 })
+
+resumeListItmes.forEach(resumeListItme=>{
+    resumeListItme.addEventListener("click" ,  function(){
+        document.querySelector(".resume-list__itme--activ").classList.remove('resume-list__itme--activ')
+        document.querySelector(".resume-content--show").classList.remove('resume-content--show')
+        resumeListItme.classList.add('resume-list__itme--activ')
+
+         let attribute = resumeListItme.getAttribute('data-content-id');
+         document.querySelector(attribute).classList.add('resume-content--show')
+        
+            
+
+    })
+
+    })
